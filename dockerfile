4e215fccce5f447032c37dfbdf8b499a329b7835
uwaheed1988 <uzirox.19@gmail.com>
2021-01-21 16:36:23 +0500

    Updated tests

--- dockerfile
+++ dockerfile
@@ -2,6 +2,6 @@ FROM python:3.9.0
 COPY . /madlib
 WORKDIR /madlib
 RUN pip3 install -r requirements.txt 
-EXPOSE 5001 
+EXPOSE 5000 
 ENTRYPOINT [ "python3.9" ] 
 CMD [ "api.py" ] 
--- test.py
+++ test.py
@@ -1,17 +1,8 @@
 import unittest
-import nltk
 from api import app
-from nltk.corpus import wordnet as wn
+from PyDictionary import PyDictionary
 
-
-def check_word(word):
-    nltk.download('wordnet')
-    pos_l = []
-    for tmp in wn.synsets(word):
-        if tmp.name().split('.')[0] == word:
-            pos_l.append(tmp.pos())
-    return pos_l
-
+dictionary=PyDictionary()
 
 class BasicTestCase(unittest.TestCase):
 
@@ -21,8 +12,8 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         sentense = response.data.decode("utf-8")
         adjective = sentense.split()[3]
-        adjective_result = check_word(adjective)
-        self.assertTrue(adjective_result != None and len(adjective_result) > 0 and 's' in adjective_result)
+        adjective_result = dictionary.meaning(adjective)
+        self.assertTrue('Adjective' in adjective_result.keys())
     
     def test_verb(self):
         tester = app.test_client(self)
@@ -30,8 +21,8 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         sentense = response.data.decode("utf-8")
         verb = sentense.split()[13]
-        verb_result = check_word(verb)
-        self.assertTrue(verb_result != None and len(verb_result) > 0 and 'v' in verb_result)
+        verb_result = dictionary.meaning(verb)
+        self.assertTrue('Verb' in verb_result.keys())
     
     def test_noun(self):
         tester = app.test_client(self)
@@ -39,8 +30,8 @@ class BasicTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         sentense = response.data.decode("utf-8")
         noun = sentense.split()[22].replace('"','').replace('?','')
-        noun_result = check_word(noun)
-        self.assertTrue(noun_result != None and len(noun_result) > 0 and 'n' in noun_result)
+        noun_result = dictionary.meaning(noun)
+        self.assertTrue('Noun' in noun_result.keys())
 
 
 if __name__ == '__main__':
